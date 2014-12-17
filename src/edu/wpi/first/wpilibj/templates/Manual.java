094a8f7405ee53c0546c9852ad3bc187f8d1f3fa
Cody Burrows <code1010@rochester.rr.com>
2014-12-17 14:33:19 -0500

    Pre-Run changes
    We know how it werks now

--- src/edu/wpi/first/wpilibj/templates/Manual.java
+++ src/edu/wpi/first/wpilibj/templates/Manual.java
@@ -6,7 +6,7 @@
  =======================================================*/
 package edu.wpi.first.wpilibj.templates;
 
-import edu.wpi.first.wpilibj.Compressor;
+//import edu.wpi.first.wpilibj.Compressor; - Compressor cart
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
@@ -18,8 +18,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Manual extends IterativeRobot {
 
     //!\\ Should be moved into Wiring class!
-    public final int LEFT1 = 1;
-    public final int LEFT2 = 2;
+    public final int LEFT1 = 2;
+    public final int LEFT2 = 1;
     public final int RIGHT1 = 3;
     public final int RIGHT2 = 4;
 
@@ -30,23 +30,24 @@ public class Manual extends IterativeRobot {
     double ratio1;
     double ratio2;
     int maxRPM;
-    boolean parkingBrake = false;
+//    boolean parkingBrake = false;
 
     //2 left motors
     Talon l1;
-    Talon l2;
+//    Talon l2;
 
     //2 right motors
     Talon r1;
-    Talon r2;
+//    Talon r2;
 
     //controls
     Joystick joy;
-    Encoder speed;
+//    Encoder speed;
 
     //Pneumatics to control shifting
-    Compressor comp;
-    Solenoid shift;
+//    Compressor comp;
+    Solenoid shift1;
+    Solenoid shift2;
 
     //driving
     RobotDrive rd;
@@ -54,28 +55,30 @@ public class Manual extends IterativeRobot {
     public void robotInit() {
 
         l1 = new Talon(LEFT1);
-        l2 = new Talon(LEFT2);
+//        l2 = new Talon(LEFT2);
         r1 = new Talon(RIGHT1);
-        r2 = new Talon(RIGHT2);
+//        r2 = new Talon(RIGHT2);
         joy = new Joystick(1);
         //!\\ GET Correct Values for compressor - (Pressure Switch channel, Compressor Relay Channel)
-        comp = new Compressor(1, 2);
+//        comp = new Compressor(1, 2);
         gear = 1;
         teeth1 = 50;
         teeth2 = 100;
         ratio1 = 1.05;
         ratio2 = 1.99;
         maxRPM = 3000;
-        rd = new RobotDrive(l1, l2, r1, r2);
+        rd = new RobotDrive(l1, r1);
+        shift1 = new Solenoid(1);
+        shift2 = new Solenoid(2);
     }
 
     public void disabled() {
 
-        comp.stop();
+//        comp.stop();
         l1.stopMotor();
-        l2.stopMotor();
+//        l2.stopMotor();
         r1.stopMotor();
-        r2.stopMotor();
+//        r2.stopMotor();
         gear1();
 
     }
@@ -116,12 +119,14 @@ public class Manual extends IterativeRobot {
     }
 
     public void gear1() {
-        shift.set(true);
+        shift1.set(true);
+        shift2.set(false);
         System.out.println("GEAR 1");
     }
 
     public void gear2() {
-        shift.set(false);
+        shift1.set(false);
+        shift2.set(true);
         System.out.println("GEAR 2");
     }
 
